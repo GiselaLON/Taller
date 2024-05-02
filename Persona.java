b18a6f41f5702f1d874e5d5ba95972056c800a01
Gisella <gisela-jc-1993@hotmail.com>
2024-05-02 00:07:15 -0500

    Encapsulamiento - Implementacióe Clase Persona por Gisela Londoño.

---
+++ Persona.java
@@ -0,0 +1,16 @@
+/**
+ * Persona
+ */
+import java.util.*;
+public class Persona {
+
+    private String nombre;
+    private int edad;
+    private Date fechaNacimiento;
+
+
+
+
+
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Java
+Taller Java 
