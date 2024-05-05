a5c019e0a2a85383d82375e8be3cb95e51f15156
Gisella <gisela-jc-1993@hotmail.com>
2024-05-05 15:10:23 -0500

    Encapsulamiento - Implementación de Métodos para Manipulación de Propiedades por Gisella Londoño

--- Persona.java
+++ Persona.java
@@ -1,6 +1,8 @@
 /**
  * Persona
  */
+
+import java.time.LocalDate;
 import java.util.*;
 public class Persona {
 
@@ -8,11 +10,53 @@ public class Persona {
     private int edad;
     private Date fechaNacimiento;
 
-/**
+    
+    /**
  * primer commit
  */
 
 
+    /*metodo constructor */
+    public Persona(String nombre, int edad, Date fechaNacimiento) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.fechaNacimiento = fechaNacimiento;
+    }
 
     
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public int getEdad() {
+        return edad;
+    }
+
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+/**
+ * Segundo commit
+ */
+
+
+
+
 }
